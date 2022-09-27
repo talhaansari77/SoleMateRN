393d20b80592d3eb4d7e600c1ed1b335d172282e
talhaansari77 <talhaprince09@gmail.com>
2022-09-27 18:12:00 +0500

    dmLinks

--- App.js
+++ App.js
@@ -4,11 +4,43 @@ import {Provider} from 'redux';
 import {store} from './src/redux/store';
 import {useFonts} from 'react-native-font';
 import RootNavigator from './src/routes';
+import dynamicLinks from '@react-native-firebase/dynamic-links';
+import {useEffect} from 'react';
 
 LogBox.ignoreLogs(['VirtualizedLists', 'Warning:...']);
 LogBox.ignoreAllLogs();
 
 export default function App() {
+  async function buildLink() {
+    const link = await dynamicLinks().buildLink({
+      link: 'https://invertase.io',
+      // domainUriPrefix is created in your Firebase console
+      domainUriPrefix: 'https://getsolemate.page.link',
+      // optional setup which updates Firebase analytics campaign
+      // "banner". This also needs setting up before hand
+      analytics: {
+        campaign: 'banner',
+      },
+    });
+
+    return link;
+  }
+
+  //  const handleUrlLink = link => {
+  //   console.log('this is DynamicLink:🖐', link);
+  // };
+
+  useEffect(() => {
+    buildLink().then(res => console.log('this is DynamicLink:🖐', res));
+
+    // console.log('this is DynamicLink:🖐', dynamicLinks().getInitialLink());
+
+    // const linkingListener = dynamicLinks().onLink(handleUrlLink);
+    // return () => {
+    //   linkingListener();
+    // };
+  }, []);
+
   // const [loaded] = useFonts({
   //   regular: require('./assets/Fonts/ProximaNova_Regular.otf'),
   //   bold: require('./assets/Fonts/ProximaNova_Bold.otf'),
